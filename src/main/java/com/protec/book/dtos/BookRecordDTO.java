0f3728a368f4beef948e14fde1e0f991404ebd86
Alexandre Ferreira <ajferreira.br@hotmail.com>
2023-08-19 21:08:47 -0300

    feat: create BookRecordDTO

---
+++ src/main/java/com/protec/book/dtos/BookRecordDTO.java
@@ -0,0 +1,43 @@
+package com.protec.book.dtos;
+
+import com.protec.book.domain.Book;
+import com.protec.book.domain.EBookType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.beans.BeanUtils;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public record BookRecordDTO(
+       @NotNull(message = "nome não pode ser nulo")
+       @Size(max = 50, message = "Máximo 50 caracteres")
+       String nome,
+       Date dataPublicacao,
+       @Size(max = 30, message = "categoria deve ter no máximo 30 caracteres")
+       String categoria,
+        @NotNull (message = "Um valor deve ser informado")
+        BigDecimal valor,
+        @NotNull (message = "O tipo de livro deve ser informado")
+        EBookType tipo
+        ) {
+
+    public Book MapperFromDTO() {
+
+        Book book = new Book();
+       /*
+        book.setNome(this.nome);
+        book.setCategoria(this.categoria);
+        book.setDataPublicacao(this.dataPublicacao);
+        book.setValor(this.valor);
+        book.setTipo(this.tipo);
+        */
+
+        BeanUtils.copyProperties(this, book);
+
+        return book;
+
+    }
+
+
+}
