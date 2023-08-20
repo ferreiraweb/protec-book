2ea46e7ec8fb2dce4b11e5444bdeee5c17186b2c
Alexandre Ferreira <ajferreira.br@hotmail.com>
2023-08-20 19:48:30 -0300

    feat: add IdMustBeNonNullException

---
+++ src/main/java/com/protec/book/exceptions/IdMustBeNonNullException.java
@@ -0,0 +1,10 @@
+package com.protec.book.exceptions;
+
+public class IdMustBeNonNullException extends RuntimeException {
+
+    private static final Long serialVersionUID = 1L;
+
+    public IdMustBeNonNullException(String message) {
+        super(message);
+    }
+}
