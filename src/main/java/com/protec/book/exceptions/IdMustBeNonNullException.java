6ea84fabd5b6eed2f3a74028179238e272435c39
Alexandre Ferreira <ajferreira.br@hotmail.com>
2023-08-21 12:32:19 -0300

    feat: edit class IdMustBeNonNullException

--- src/main/java/com/protec/book/exceptions/IdMustBeNonNullException.java
+++ src/main/java/com/protec/book/exceptions/IdMustBeNonNullException.java
@@ -1,7 +1,6 @@
 package com.protec.book.exceptions;
 
 public class IdMustBeNonNullException extends RuntimeException {
-
     private static final Long serialVersionUID = 1L;
 
     public IdMustBeNonNullException(String message) {
